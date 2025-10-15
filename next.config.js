d1f39b3245a23a4b0826e5f495b9bb030f3e215a
Nyxode <103940417+Nyxode@users.noreply.github.com>
2025-10-15 17:16:48 +0900

    fix: ensure static export for firebase

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 静的出力にする（Firebase Hosting用）
   output: 'export',
+
+  // 画像最適化を無効化（Next Imageが使えないため）
   images: {
     unoptimized: true,
   },
+
+  // 型エラーでビルドを止めない
   typescript: {
-    // 型エラーがあってもビルドを止めない
     ignoreBuildErrors: true,
   },
 };
